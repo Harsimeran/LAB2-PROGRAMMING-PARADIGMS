fd81f3cc511465f9e65adad4f0463fa524add43c
Harsimeran <simranchahal16271@gmail.com>
2023-11-24 10:28:13 -0500

    version1

--- Program.fs
+++ Program.fs
@@ -1,4 +1,48 @@
-﻿type Cuisine = 
+﻿type Coach = {
+    Name: string
+    FormerPlayer: bool
+}
+
+type Stats = {
+    Wins: int
+    Losses: int
+}
+
+type Team = {
+    Name: string
+    Coach: Coach
+    Stats: Stats
+}
+
+//Create a list of 5 teams 
+let teams : Team list = [
+    { Name = "Houston Rockets"; Coach = { Name = "Ime Udoka"; FormerPlayer = true }; Stats = { Wins = 2328; Losses = 2196 } }
+    { Name = "Milwaukee Bucks"; Coach = { Name = "Adrian Griffin"; FormerPlayer = false }; Stats = { Wins = 2340; Losses = 2103 } }
+    { Name = "Los Angeles Lakers"; Coach = { Name = "Frank Vogel"; FormerPlayer = false }; Stats = { Wins = 2400; Losses = 2200 } }
+    { Name = "Miami Heat"; Coach = { Name = "Erik Spoelstra"; FormerPlayer = true }; Stats = { Wins = 2400; Losses = 2000 } }
+    { Name = "LA Clippers"; Coach = { Name = "Taylor Lue"; FormerPlayer = true }; Stats = { Wins = 1792; Losses = 2486 } }
+]
+
+//filtering the list
+let goodTeamNames =
+    teams
+    |> List.filter (fun team -> team.Stats.Wins > team.Stats.Losses)
+    |> List.map (fun team -> team.Name)
+
+printfn "Good Teams: %A" goodTeamNames
+
+//Mapping the list
+let calculateWinningPercentage team =
+    float team.Stats.Wins / float (team.Stats.Wins + team.Stats.Losses) * 100.0
+
+let bestTeam =
+    teams |> List.maxBy calculateWinningPercentage
+
+printfn "Best Team by Winning Percentage: %s" bestTeam.Name
+printfn "Winning Percentage: %.2f%%" (calculateWinningPercentage bestTeam)
+
+//Discriminated Union
+type Cuisine = 
 | Korean
 | Turkish
 
